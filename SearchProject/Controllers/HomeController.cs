229c9abb1923a5408f64420ce4455958a0b72b46
Brandon <brandondetra@outlook.com>
2018-06-27 21:21:58 -0600

    Removed Unnecessary Files, Updated the Home Controller and the Analyze ViewModel and Cleaned the Home and Shared View.

--- SearchProject/Controllers/HomeController.cs
+++ SearchProject/Controllers/HomeController.cs
@@ -1,37 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using SearchProject.Models;
+using TwitterSearcher.Models;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
 
 namespace SearchProject.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            //On get, Validate user, create new viewmodel and display.
+
+            Auth.SetUserCredentials("7lYht3bDy2P9ji0SXLl66feKi", 
+                                    "k9hITdO7MmzeT57xTg75GrtneYjgwglXvoAawQ98XkPE21KMPB", 
+                                    "963967640753655809-ZMrjjJP4F7lxCR94DUetJkROY6f1gpx", 
+                                    "ILZMtk7b6lk4dBdekuJz5AvN9NPCghBw4IkqOVCTwuBfT");
+
+            AnalyzeViewModel newAnalyzeViewModel = new AnalyzeViewModel();
+
+            return View(newAnalyzeViewModel);
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public IActionResult Index(AnalyzeViewModel newAnalyzeViewModel)
         {
-            ViewData["Message"] = "Your application description page.";
+            //On Post, Verify the model and display data with form. If model not valid, refresh the page.
+            if (ModelState.IsValid)
+            {
+                //Counter holds all accounted values.
+                Dictionary<string, int> counter = new Dictionary<string, int>();
 
-            return View();
-        }
+                //Content holds string arrays of all incoming tweets.
+                List<string[]> content = new List<string[]>();
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+                
+                /***Create a tweetinvi searchparameter in english, 
+                 * that pulls both popular and recent posts and 
+                 * sets the number of results to the user's sample size.
+                 ***/
+                var searchParameter = new SearchTweetsParameters(newAnalyzeViewModel.Keyword)
+                {
+                    Lang = LanguageFilter.English,
+                    SearchType = SearchResultType.Mixed,
+                    MaximumNumberOfResults = newAnalyzeViewModel.SampleSize,
+                };
 
-            return View();
-        }
+                //All posts are pulled here and added to the content list.
+                var tweets = Search.SearchTweets(searchParameter);
+                foreach (var tweet in tweets)
+                {
+                    content.Add(tweet.Text.Split());
+                }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+                //Each word and it's frequency is calculated and added to the Counter Dictionary.
+                foreach (string[] text in content)
+                {
+                    foreach (string word in text)
+                    {
+                        if (counter.ContainsKey(word))
+                        {
+                            counter[word]++;
+                        }
+                        else
+                        {
+                            counter.Add(word, 1);
+                        }
+                    }
+                }
+
+                //Finally the ViewModel's Counter is set to the function's and the data is displayed in the view.
+                newAnalyzeViewModel.Counter = counter;
+                return View(newAnalyzeViewModel);
+            }
+            else
+            {
+                //If there is no input, the page is refreshed.
+                return View(newAnalyzeViewModel);
+            }
         }
     }
 }
---
+++ SearchProject/ViewModels/AnalyzeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitterSearcher.Models
+{
+    public class AnalyzeViewModel
+    {
+        [Required]
+        public string Keyword { get; set; }
+
+        [Required]
+        public int SampleSize { get; set; }
+
+        public Dictionary<string, int> Counter { get; set; }
+
+        public AnalyzeViewModel()
+        {
+        }
+    }
+}
